044a80d4cee8ab17cb47a80bd32459d92611ed34
Joosep Mart Männik <joosep.mannik@gmail.com>
2025-12-19 20:25:14 +0200

    version 1.1.0

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import json
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -11,23 +10,29 @@ def hello_world():  # put application's code here
 
 @app.route('/api/decision', methods=["GET", "POST"])
 def accept_week_data():
-    print(request.form)
-    if "handshake" in request.form and request.form.get("handshake") is True:
+    data = request.json
+    print("Got the following request")
+    print(request.headers.get("X-Forwarded-For"))
+    print(request.remote_addr)
+    print(data)
+    if "handshake" in data and data.get("handshake") is True:
         return {
             "ok": True,
             "student_email": "jomann@taltech.ee",
             "algorithm_name": "JoosepDrinksBeer",
-            "version": "v0.1.1",
+            "version": "v1.0.0",
             "supports": {"blackbox": True, "glassbox": False},
             "message": "BeerBot ready"
         }
-    if "weeks" in request.form:
-        weeks : list[dict] = json.loads(request.form.get("weeks"))
+    if "weeks" in data:
+        weeks : list[dict] = data.get("weeks")
         current_week = weeks[-1]
         orders = {}
-        for role, amounts in current_week['roles']:
+        for role, amounts in current_week['roles'].items():
             order = amounts['incoming_orders']
             orders[role] = order
+        print("Returning the following orders")
+        print(orders)
         return {
             "orders": orders
         }
