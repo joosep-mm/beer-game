e5e8b45ba519cda6f53397ae3068057565dc2834
Joosep Mart Männik <joosep.mannik@gmail.com>
2025-12-21 22:41:57 +0200

    using average demand. v2.1.0

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def accept_week_data():
             "ok": True,
             "student_email": "jomann@taltech.ee",
             "algorithm_name": "JoosepDrinksBeer",
-            "version": "v2.0.0",
+            "version": "v2.1.0",
             "supports": {"blackbox": True, "glassbox": False},
             "message": "BeerBot ready"
         }
@@ -39,14 +39,17 @@ def accept_week_data():
 
         current_week = weeks[-1]
         orders = {}
+        average_demands = find_average_demand(weeks)
+
         for role, amounts in current_week['roles'].items():
             demand = amounts['incoming_orders']
             inventory = amounts['inventory']
             backlog = amounts['backlog']
             in_transit = sum(amounts['in_transit'])
 
+            avg_demand = average_demands[role]
             delay = ROLE_DELAY[role]
-            target = demand * delay
+            target = avg_demand * delay
 
             inventory_position = inventory + in_transit - backlog
             random.seed(seed)
@@ -54,7 +57,7 @@ def accept_week_data():
             order = demand + alpha * (target - inventory_position)
 
             order = max(0, int(round(order)))
-            order = min(order, 2 * demand)
+            order = min(order, 2 * avg_demand)
 
             orders[role] = order
         print("Returning the following orders")
@@ -66,6 +69,21 @@ def accept_week_data():
     return {
         "test": True
     }
+
+
+def find_average_demand(weeks) -> dict[str, int]:
+    result = {}
+    count = 0
+    for week in weeks:
+        count += 1
+        for role, amounts in week['roles'].items():
+            demand = amounts['incoming_orders']
+            result[role] = result.get(role, 0) + demand
+
+    for key, value in result.items():
+        result[key] = value // count
+
+    return result
 
 if __name__ == '__main__':
     app.run()
