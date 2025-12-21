410b0eabc59c0c2ac174c19d3235a22a6b5a61bc
Joosep Mart Männik <joosep.mannik@gmail.com>
2025-12-21 21:58:04 +0200

    new and improved. version 2

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+import random
+
 from flask import Flask, request
 
+REQUIRED_INVENTORY = 20
+ROLE_DELAY = {
+    'retailer': 2,
+    'distributor': 3,
+    'wholesaler': 3,
+    'factory': 4
+}
 app = Flask(__name__)
 
 
@@ -20,16 +29,34 @@ def accept_week_data():
             "ok": True,
             "student_email": "jomann@taltech.ee",
             "algorithm_name": "JoosepDrinksBeer",
-            "version": "v1.0.0",
+            "version": "v2.0.0",
             "supports": {"blackbox": True, "glassbox": False},
             "message": "BeerBot ready"
         }
     if "weeks" in data:
         weeks : list[dict] = data.get("weeks")
+        seed = data.get('seed', 2025)
+
         current_week = weeks[-1]
         orders = {}
         for role, amounts in current_week['roles'].items():
-            order = amounts['incoming_orders']
+            demand = amounts['incoming_orders']
+            inventory = amounts['inventory']
+            backlog = amounts['backlog']
+            arriving = amounts['arriving']
+            in_transit = sum(amounts['in_transit'])
+
+            delay = ROLE_DELAY[role]
+            target = demand * delay
+
+            inventory_position = inventory + in_transit - backlog
+            random.seed(seed)
+            alpha = random.uniform(0.2, 0.4)
+            order = demand + alpha * (target - inventory_position)
+
+            order = max(0, int(round(order)))
+            order = min(order, 2 * demand)
+
             orders[role] = order
         print("Returning the following orders")
         print(orders)
